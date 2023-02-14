cc68a04924211d6d913658994860805412130d38
JackLabukas <labukasgediminas@gmail.com>
2023-02-14 20:27:22 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
 import React from "react";
-import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return null;
 }
 
 export default App;
--- src/pages/Home.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Home() {
-  return <div>HELLO</div>;
-}
-
-export default Home;
