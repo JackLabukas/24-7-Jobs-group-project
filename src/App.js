8299190796203b472068de3ca88c52d04363cd17
JackLabukas <labukasgediminas@gmail.com>
2023-02-16 21:06:32 +0000

    added salary API

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import JobList from "./components/JobList";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Home from "./pages/Home";
+import Salary from "./components/Salary";
 
 const testArr = JSON.parse(localStorage.getItem("Response"));
 const salaryArr = JSON.parse(localStorage.getItem("Salary"));
@@ -15,6 +15,7 @@ function App() {
           path="/"
           element={
             <>
+              <Salary salaryArr={salaryArr} />
               <JobList testArr={testArr} />
             </>
           }
--- src/components/Salary.js
+++ src/components/Salary.js
@@ -2,18 +2,17 @@ import React from "react";
 
 function Salary(props) {
   return (
-    <div class="card text-center">
-      <div class="card-header">Featured</div>
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">
+    <div className="card text-center">
+      <div className="card-header"></div>
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">
           With supporting text below as a natural lead-in to additional content.
         </p>
-        <a href="#" class="btn btn-primary">
-          Go somewhere
-        </a>
       </div>
-      <div class="card-footer text-muted">2 days ago</div>
+      <div className="card-footer text-muted">2 days ago</div>
     </div>
   );
 }
+
+export default Salary;
