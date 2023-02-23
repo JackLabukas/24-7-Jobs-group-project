e6df8fcbbd31a72f25310bb8d92fb6a3a07e614d
KE-L <kassy.g.e@hotmail.com>
2023-02-23 16:10:09 +0000

    added button functionality

--- src/components/Navbar/Navbar1.js
+++ src/components/Navbar/Navbar1.js
@@ -90,6 +90,26 @@ function NavBar1() {
           >
             About
           </motion.a>
+          <motion.a
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              default: {
+                duration: 0.3,
+                ease: [0, 0.71, 0.2, 1.01],
+              },
+              scale: {
+                type: "spring",
+                damping: 5,
+                stiffness: 100,
+                restDelta: 0.001,
+              },
+            }}
+            className="nav-item nav-link text-light text-uppercase"
+            href="/team"
+          >
+            Team
+          </motion.a>
 
           <div className="nav-item nav-link text-light" onClick={toggleTheme}>
             {darkTheme ? <BsSun /> : <BsMoonStars />}
