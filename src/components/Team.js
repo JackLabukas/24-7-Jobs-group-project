6d7e030c3119b43844a7eb077ca4b9173822dfad
KE-L <kassy.g.e@hotmail.com>
2023-02-23 16:26:08 +0000

    added 3 cards on team page

--- src/components/Team.js
+++ src/components/Team.js
@@ -3,23 +3,55 @@ import React from "react";
 function Team() {
 
     return (
+
         <div className="container">
             <div className="row">
-                <div className="col-12 col-sm-8 col-md-6 col-lg-4">
-                    <div className="card">
+                <div className="col-lg-4">
+                    <div className="card m-4">
                         <img className="card-img-top" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/oslo.jpg" alt="Bologna"></img>
                         <div className="card-body text-center">
                             <img className="avatar rounded-circle" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/robert.jpg" alt="Bologna"></img>
-                            <h4 className="card-title">Robert Downey Jr.</h4>
-                            <h6 className="card-subtitle mb-2 text-muted">Famous Actor</h6>
-                            <p className="card-text">Robert John Downey Jr.'career has included critical and popular success in his youth, followed by a period of substance abuse and legal difficulties, and a resurgence of commercial success in middle age. </p>
-                            <a href="#" className="btn btn-info">Follow</a>
-                            <a href="#" className="btn btn-outline-info">Message</a>
+                            <h4 className="card-title">Jack</h4>
+                            <h6 className="card-subtitle mb-2 text-muted">Developer</h6>
+                            <p className="card-text"> </p>
+                            <a href="#" className="btn btn-info">https://uk.linkedin.com/in/jack-labukas-5bb038b7</a>
+                            <a href="#" className="btn btn-outline-info">https:github.com/JackLabukas</a>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+                <div className="col-lg-4">
+                    <div className="card m-4">
+                        <img className="card-img-top" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/oslo.jpg" alt="Bologna"></img>
+                        <div className="card-body text-center">
+                            <img className="avatar rounded-circle" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/robert.jpg" alt="Bologna"></img>
+                            <h4 className="card-title">Jack</h4>
+                            <h6 className="card-subtitle mb-2 text-muted">Developer</h6>
+                            <p className="card-text"> </p>
+                            <a href="#" className="btn btn-info">https://uk.linkedin.com/in/jack-labukas-5bb038b7</a>
+                            <a href="#" className="btn btn-outline-info">https:github.com/JackLabukas</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4">
+                    <div className="card m-4">
+                        <img className="card-img-top" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/oslo.jpg" alt="Bologna"></img>
+                        <div className="card-body text-center">
+                            <img className="avatar rounded-circle" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/robert.jpg" alt="Bologna"></img>
+                            <h4 className="card-title">Jack</h4>
+                            <h6 className="card-subtitle mb-2 text-muted">Developer</h6>
+                            <p className="card-text"> </p>
+                            <a href="#" className="btn btn-info">https://uk.linkedin.com/in/jack-labukas-5bb038b7</a>
+                            <a href="#" className="btn btn-outline-info">https:github.com/JackLabukas</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     );
 };
 
