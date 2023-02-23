9279b61f7eab4c1217b1b840e614a81362cddc07
KE-L <kassy.g.e@hotmail.com>
2023-02-23 16:34:06 +0000

    added images to team cards

--- src/components/Team.js
+++ src/components/Team.js
@@ -10,7 +10,7 @@ function Team() {
                     <div className="card m-4">
                         <img className="card-img-top" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/oslo.jpg" alt="Bologna"></img>
                         <div className="card-body text-center">
-                            <img className="avatar rounded-circle" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/robert.jpg" alt="Bologna"></img>
+                            <img className="avatar rounded-circle" src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=740&t=st=1677169914~exp=1677170514~hmac=f81f0ea8241ff0ff685142c5dca7f0a2d0373690ca22daeb2d69f28baf1de218?w=200&h=200" alt="Bologna"></img>
                             <h4 className="card-title">Jack</h4>
                             <h6 className="card-subtitle mb-2 text-muted">Developer</h6>
                             <p className="card-text"> </p>
@@ -27,7 +27,7 @@ function Team() {
                     <div className="card m-4">
                         <img className="card-img-top" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/oslo.jpg" alt="Bologna"></img>
                         <div className="card-body text-center">
-                            <img className="avatar rounded-circle" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/robert.jpg" alt="Bologna"></img>
+                            <img className="avatar rounded-circle" src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=740&t=st=1677169914~exp=1677170514~hmac=f81f0ea8241ff0ff685142c5dca7f0a2d0373690ca22daeb2d69f28baf1de218?w=200&h=200" alt="Bologna"></img>
                             <h4 className="card-title">Jack</h4>
                             <h6 className="card-subtitle mb-2 text-muted">Developer</h6>
                             <p className="card-text"> </p>
@@ -40,7 +40,7 @@ function Team() {
                     <div className="card m-4">
                         <img className="card-img-top" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/oslo.jpg" alt="Bologna"></img>
                         <div className="card-body text-center">
-                            <img className="avatar rounded-circle" src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/robert.jpg" alt="Bologna"></img>
+                            <img className="avatar rounded-circle" src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=740&t=st=1677169914~exp=1677170514~hmac=f81f0ea8241ff0ff685142c5dca7f0a2d0373690ca22daeb2d69f28baf1de218?w=200&h=200" alt="Bologna"></img>
                             <h4 className="card-title">Jack</h4>
                             <h6 className="card-subtitle mb-2 text-muted">Developer</h6>
                             <p className="card-text"> </p>
